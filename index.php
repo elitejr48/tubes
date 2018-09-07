66278765285a6e0cfed6610cb72c683b3c9d5891
Yudha Pratama <elitejr48@gmail.com>
2018-09-07 19:24:47 +0700

    footer done

--- index.php
+++ index.php
@@ -171,7 +171,49 @@
     	</div>
     </div>
   </div>
-
+  <div class="footer">
+      <div class="contain">
+      <div class="col">
+        <h1>Home</h1>
+        <ul>
+          <li>About</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h1>Products</h1>
+        <ul>
+          <li>About</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h1>Accounts</h1>
+        <ul>
+          <li>About</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h1>Resources</h1>
+        <ul>
+          <li>Webmail</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h1>Support</h1>
+        <ul>
+          <li>Contact us</li>
+        </ul>
+      </div>
+      <div class="col social">
+        <h1>Social</h1>
+        <ul>
+          <li><img src="https://svgshare.com/i/5fq.svg" width="32" style="width: 32px;"></li>
+          <li><img src="https://svgshare.com/i/5eA.svg" width="32" style="width: 32px;"></li>
+          <li><img src="https://svgshare.com/i/5f_.svg" width="32" style="width: 32px;"></li>
+        </ul>
+      </div>
+    <div class="clearfix"></div>
+    </div>
+  </div>
 
       <script>
       function myFunction() {
