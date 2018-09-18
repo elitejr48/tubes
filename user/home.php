6003ecdf758daca4b11bdb3e496319774b35f718
Mikael Dio K <mikzpong@gmail.com>
2018-09-18 20:07:01 +0700

    ganti lagi

--- user/home.php
+++ user/home.php
@@ -31,7 +31,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-              <a class="navbar-brand" href="http://disputebills.com"><img src="logo 1 edit.png" alt="Dispute Bills" style="max-height: 155%" >
+              <a class="navbar-brand" href="http://disputebills.com"><img src="../logo 1 edit.png" alt="Dispute Bills" style="max-height: 155%" >
             </a>
           </div>
           <div id="navbar3" class="navbar-collapse collapse">
