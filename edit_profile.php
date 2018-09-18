956e1d143b6671934055ea8c32f5666fc7e3092d
Mikael Dio K <mikzpong@gmail.com>
2018-09-18 16:15:57 +0700

    itu css yang bener buat edit profile

--- edit_profile.php
+++ edit_profile.php
@@ -7,7 +7,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="reservation.css">
+  <link rel="stylesheet" type="text/css" href="edit_profile.css">
   <script>
   $(document).ready(function(){
       $("#myBt").click(function(){
@@ -87,6 +87,8 @@
 
 <div class="background" >
   <div class="transbox" align="center" style="background:gray;opacity:0.9" >
+  <h2>EDIT PROFILE</h2>
+  <br>
 	<form action="edit-proses.php" method="post">
 		<input type="hidden" name="id" value="<?php echo $id; ?>">
 							<table>
