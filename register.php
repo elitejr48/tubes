a6015dbcc180119b50b0dbf489599b7f48f24d5b
dionisiusyudha <43060149+dionisiusyudha@users.noreply.github.com>
2018-09-27 15:53:52 +0700

    regis

--- register.php
+++ register.php
@@ -178,7 +178,7 @@
     $('#password, #confirm_password').on('keyup', function () {
     if ($('#password').val() == $('#confirm_password').val()) {
         $('#message').html('Matching').css('color', 'green');
-    } else 
+    } else
         $('#message').html('Not Matching').css('color', 'red');
     });
 </script>
